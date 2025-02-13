519b1e2bddaa91ab25f9655470ec14fb6ad72b90
Acero Liang <acero.liangli@stonybrook.edu>
2025-02-13 12:06:06 -0500

    second

--- convert.py
+++ convert.py
@@ -1,10 +1,10 @@
 import cairosvg
 from PIL import Image
 
-# Convert SVG to PNG
-cairosvg.svg2png(url="SCB.svg", write_to="temp.png")
 icon_width = 128
 icon_height = 256
+# Convert SVG to PNG
+cairosvg.svg2png(url="SCB.svg", write_to="temp.png", output_width=icon_width, output_height=icon_height)
 
 # Convert PNG to ICO
 img = Image.open("temp.png")
